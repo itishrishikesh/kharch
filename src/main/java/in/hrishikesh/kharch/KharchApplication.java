44deb3f4569cf3e29c1d7e1642da42c6e5091e26
itishrishikesh <itishrishikesh@gmail.com>
2021-12-03 16:35:52 +0530

    initial commit

---
+++ src/main/java/in/hrishikesh/kharch/KharchApplication.java
@@ -0,0 +1,13 @@
+package in.hrishikesh.kharch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KharchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KharchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/hrishikesh/kharch/KharchApplicationTests.java
@@ -0,0 +1,13 @@
+package in.hrishikesh.kharch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KharchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
